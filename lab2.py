7081e884a5cee5d126ed5c0b1f07e83293236247
romen <rw1019705@wcupa.edu>
2024-02-14 19:44:12 -0500

    work

--- lab2.py
+++ lab2.py
@@ -5,13 +5,14 @@ print("----------------------")
 price = float(input("Enter price of food: $"))
 
 # print "Enter tax rate in percent (e.g. .08 for 8%):"
-taxRate = float(input("Enter tax rate in percent (e.g. .08 for 8%):"))
+taxRate = float(input("Enter tax rate in percent (e.g. .08 for 8%): "))
 # end of user input
 
 
 # compute tax and total
 tax = price * taxRate
 total = price + tax
+
 
 # output final numbers
 print("Tax on food is $" + str(tax))
